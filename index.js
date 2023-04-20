4be4cd70416d071ead6f3d6cd1c7c02831036abe
Kevin Avelino <avesanke@gmail.com>
2023-04-20 11:16:56 -0600

    Fixed errors and added command npm start

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const env = require("dotenv").config()
 const express = require("express")
 const app = express()
+const PORT = process.env.PORT || 8080
 
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
 
 app.post("/create-setup-intent", async (req, res) => {
-    const { id } = await stripe.costumers.create()
+    const { id } = await stripe.customers.create()
 
-    const { client_secret } = await stripe.setupIntent.create({costumer: id})
+    const { client_secret } = await stripe.setupIntents.create({ customer: id })
 
     res.send({ id, client_secret })
 })
@@ -16,4 +17,4 @@ app.get("/public-key", (req, res) => {
     res.send({ publicKey: process.env.STRIPE_PUBLISHABLE_KEY });
 })
 
-app.listen(4242, () => console.log(`Node server listening on port ${4242}!`));
+app.listen(PORT, () => console.log(`Node server listening on port ${PORT}!`));
